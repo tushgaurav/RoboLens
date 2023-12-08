1e8e48da2709da3b954d16e069da91a812df15b7
Tushar Gaurav <tushar.gaurav416@gmail.com>
2023-12-08 13:02:40 +0530

    next config changed to initial

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 const path = require("path");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "standalone",
-  reactStrictMode: true,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../../"),
-    esmExternals: false,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
